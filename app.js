79f51dd7cdcdeb80610f1629b9f10e16249ea546
Akanksha5357 <156253285+Akanksha5357@users.noreply.github.com>
2024-04-03 13:33:28 -0500

    changes made

--- app.js
+++ app.js
@@ -29,56 +29,58 @@ app.use('/juice', juiceRouter); //juice endpoint added
 app.use('/grid', gridRouter); //grid endpoint added
 app.use('/pick', pickRouter); //pick endpoint added
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
-
+var juice = require("./models/juice");
 require('dotenv').config();
-const connectionString = process.env.MONGO_CON
-mongoose = require('mongoose');
+var mongoose = require('mongoose');
+const connectionString = process.env.MONGO_CON;
 mongoose.connect(connectionString);
 //Get the default connection
 var db = mongoose.connection;
 //Bind connection to error event
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 db.once("open", function(){
-console.log("Connection to DB succeeded")});
-// We can seed the collection if needed on
-
+console.log("Connection to DB succeeded");
+ 
+});
+ 
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+ 
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+ 
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+ 
+module.exports = app;
+ 
 async function recreateDB(){
   // Delete everything
   await juice.deleteMany();
-  
+ 
   let instance1 = new juice({juice_flavor: 'Apple', juice_brand: 'AppyFizz', juice_price: 2.5 });
   instance1.save().then(doc=>{
   console.log("First object saved")}
   ).catch(err=>{
   console.error(err)
   });
-  
-  
+ 
+ 
   let instance2 = new juice({juice_flavor: 'Mango', juice_brand: 'Maaza', juice_price: 2.0});
   instance2.save().then(doc=>{
   console.log("Second object saved")}
   ).catch(err=>{
   console.error(err)
   });
-  
-  
+ 
+ 
   let instance3 = new juice({juice_flavor: 'Orange', juice_brand: 'Fanta', juice_price: 3.0});
   instance3.save().then(doc=>{
   console.log("Third object saved")}
@@ -88,3 +90,5 @@ async function recreateDB(){
   }
   let reseed = true;
   if (reseed) {recreateDB();}
+ 
+ 
