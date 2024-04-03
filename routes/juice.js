45125a4bb3f7cffd97afc84f3f2ceabf26c1ee36
Akanksha5357 <156253285+Akanksha5357@users.noreply.github.com>
2024-04-03 15:47:04 -0500

    changes made

--- routes/juice.js
+++ routes/juice.js
@@ -1,6 +1,6 @@
 var express = require('express');
-const costume_controlers= require('../controllers/juice');
+const juice_controlers= require('../controllers/juice');
 var router = express.Router();
 /* GET costumes */
-router.get('/', juice_controlers.juice_view_all_Page );
+router.get('/',juice_controlers.juice_view_all_Page );
 module.exports = router;
