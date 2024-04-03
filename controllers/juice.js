36f89c61407397cd9cff8ab30c5c719aec717d0e
Akanksha5357 <156253285+Akanksha5357@users.noreply.github.com>
2024-04-03 14:26:36 -0500

    changes made

--- controllers/juice.js
+++ controllers/juice.js
@@ -19,4 +19,18 @@ exports.juice_delete = function(req, res) {
 exports.juice_update_put = function(req, res) {
  res.send('NOT IMPLEMENTED: juice update PUT' + req.params.id);
 };
+
+
+// List of all juice
+exports.juice_list = async function(req, res) {
+    try{
+    thedress = await juice.find();
+    res.send(thedress);
+    }
+    catch(err){
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+    } 
+   };
+   
  
