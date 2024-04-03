3685723933b6d270b687a2018dff37ad42100bf2
Akanksha5357 <156253285+Akanksha5357@users.noreply.github.com>
2024-04-03 16:06:42 -0500

    changes made

--- controllers/juice.js
+++ controllers/juice.js
@@ -1,48 +1,47 @@
 var juice = require('../models/juice');
 // List of all juice
-exports.juice_list = function(req, res) {
- res.send('NOT IMPLEMENTED: juice list');
+exports.juice_list = function (req, res) {
+    res.send('NOT IMPLEMENTED: juice list');
 };
 // for a specific juice.
-exports.juice_detail = function(req, res) {
- res.send('NOT IMPLEMENTED: juice detail: ' + req.params.id);
+exports.juice_detail = function (req, res) {
+    res.send('NOT IMPLEMENTED: juice detail: ' + req.params.id);
 };
 // Handle juice create on POST.
-exports.juice_create_post = function(req, res) {
- res.send('NOT IMPLEMENTED: food create POST');
+exports.juice_create_post = function (req, res) {
+    res.send('NOT IMPLEMENTED: food create POST');
 };
 // Handle juice delete from on DELETE.
-exports.juice_delete = function(req, res) {
- res.send('NOT IMPLEMENTED: juice delete DELETE ' + req.params.id);
+exports.juice_delete = function (req, res) {
+    res.send('NOT IMPLEMENTED: juice delete DELETE ' + req.params.id);
 };
 // Handle juice update form on PUT.
-exports.juice_update_put = function(req, res) {
- res.send('NOT IMPLEMENTED: juice update PUT' + req.params.id);
+exports.juice_update_put = function (req, res) {
+    res.send('NOT IMPLEMENTED: juice update PUT' + req.params.id);
 };
 
 
 // List of all juice
-exports.juice_list = async function(req, res) {
-    try{
-    thedress = await juice.find();
-    res.send(thedress);
+exports.juice_list = async function (req, res) {
+    try {
+        thedress = await juice.find();
+        res.send(thedress);
     }
-    catch(err){
-    res.status(500);
-    res.send(`{"error": ${err}}`);
-    } 
-   };
-     
-     
-    exports.toys_view_all_Page = async function(req, res) {
-        try{
-        thejuice = await juice.find();
-        res.render('juice', { title: 'toys Search Results', results: thejuice });
-        }
-        catch(err){
+    catch (err) {
         res.status(500);
         res.send(`{"error": ${err}}`);
-        }
-        };
-   
- 
+    }
+};
+
+exports.juice_view_all_Page = async function (req, res) {
+    try {
+        thejuice = await juice.find();
+        res.render('juice', { title: 'toys Search Results', results: thejuice });
+    }
+    catch (err) {
+        res.status(500);
+        res.send(`{"error": ${err}}`);
+    }
+};
+
+
--- routes/juice.js
+++ routes/juice.js
@@ -1,6 +1,6 @@
 var express = require('express');
 const juice_controlers= require('../controllers/juice');
 var router = express.Router();
-/* GET costumes */
+/* GET juice */
 router.get('/',juice_controlers.juice_view_all_Page );
 module.exports = router;
