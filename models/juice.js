cd3db15781a149f8cca7af9f95c813aaa37778b9
Akanksha5357 <156253285+Akanksha5357@users.noreply.github.com>
2024-04-14 15:30:29 -0500

    Done Chnages

--- models/juice.js
+++ models/juice.js
@@ -1,12 +1,16 @@
 const mongoose = require("mongoose");
 
 const juiceSchema = mongoose.Schema({
-    juice_flavor: String,
+    juice_flavor: {
+        type: String,
+        required: true,
+        match: /^[a-zA-Z]+$/
+      },
     juice_brand: String,
     juice_price: {
         type: Number,
-        min: [1, "Price must be greater than or equal to 1"], // Adjusted minimum value
-        max: [1000, "Price cannot be greater than 1000"] // Adjusted maximum value
+        min: [1.5, "Price must be greater than or equal to 1"], // Adjusted minimum value
+        max: [10, "Price cannot be greater than 1000"] // Adjusted maximum value
     }
 });
 
--- routes/juice.js
+++ routes/juice.js
@@ -1,36 +1,27 @@
 var express = require('express');
-const juice_controlers= require('../controllers/juice');
 var router = express.Router();
 var passport = require('passport');
-/* GET juice */
-router.get('/',juice_controlers.juice_view_all_Page );
+const juice_controlers = require('../controllers/juice');
 
-/* GET detail juice page */
-router.get('/detail', juice_controlers.juice_view_one_Page);
-
-/* GET create juice page */
-router.get('/create', juice_controlers.juice_create_Page);
-
-// A little function to check if we have an authorized user and continue on or
-// redirect to login.
 const secured = (req, res, next) => {
-    if (req.user){
-    return next();
+    if (req.user) {
+        return next();
     }
     res.redirect("/login");
-    }
+};
 
-/* GET update juice page */
-router.get('/update', secured,juice_controlers.juice_update_Page);
-router.post('/login',passport.authenticate('local'), function(req, res) {
-    res.redirect('/');
-    });
 
-/* GET create update page */
-router.get('/update', juice_controlers.juice_update_Page);
+router.get('/', juice_controlers.juice_view_all_Page);
 
-/* GET delete juice page */
-router.get('/delete', juice_controlers.juice_delete_Page);
 
+router.get('/detail', secured, juice_controlers.juice_view_one_Page);
+
+router.get('/create', juice_controlers.juice_create_Page);
+
+
+router.get('/update', secured, juice_controlers.juice_update_Page);
+
+
+router.get('/delete', secured, juice_controlers.juice_delete_Page);
 
 module.exports = router;
