a1f2e7f47cfd08c0211af74249db69fe5d4615ab
Akanksha5357 <156253285+Akanksha5357@users.noreply.github.com>
2024-04-13 19:41:53 -0500

    validation failure

--- models/juice.js
+++ models/juice.js
@@ -1,8 +1,13 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
+
 const juiceSchema = mongoose.Schema({
-juice_flavor: String,
-juice_brand: String,
-juice_price: Number
-})
-module.exports = mongoose.model("juice",
-juiceSchema)
+    juice_flavor: String,
+    juice_brand: String,
+    juice_price: {
+        type: Number,
+        min: [1, "Price must be greater than or equal to 1"], // Adjusted minimum value
+        max: [1000, "Price cannot be greater than 1000"] // Adjusted maximum value
+    }
+});
+
+module.exports = mongoose.model("juice", juiceSchema);
--- routes/juice.js
+++ routes/juice.js
@@ -1,6 +1,7 @@
 var express = require('express');
 const juice_controlers= require('../controllers/juice');
 var router = express.Router();
+var passport = require('passport');
 /* GET juice */
 router.get('/',juice_controlers.juice_view_all_Page );
 
@@ -9,6 +10,21 @@ router.get('/detail', juice_controlers.juice_view_one_Page);
 
 /* GET create juice page */
 router.get('/create', juice_controlers.juice_create_Page);
+
+// A little function to check if we have an authorized user and continue on or
+// redirect to login.
+const secured = (req, res, next) => {
+    if (req.user){
+    return next();
+    }
+    res.redirect("/login");
+    }
+
+/* GET update juice page */
+router.get('/update', secured,juice_controlers.juice_update_Page);
+router.post('/login',passport.authenticate('local'), function(req, res) {
+    res.redirect('/');
+    });
 
 /* GET create update page */
 router.get('/update', juice_controlers.juice_update_Page);
